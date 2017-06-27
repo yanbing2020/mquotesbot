0f05b6b8b85fec183bdbe415a6ae9e11909e3609
yanbing2020 <374726241@qq.com>
2017-06-27 13:53:51 +0800

    增加css方式

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy", "crawl", "fengniao-css", "-osn.json"])
+execute(["scrapy", "crawl", "fengniao-css"])
+# execute(["scrapy", "crawl", "fengniao-css", "-osn.json"])
